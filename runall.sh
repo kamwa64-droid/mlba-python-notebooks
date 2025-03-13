f6aa5090adfed4a5c43b4699acc3aaf564278441
Peter Gedeck <peter.gedeck@gmail.com>
2025-03-13 08:11:01 -0400

    Edition 2 chapter notebooks (#2)

---
+++ runall.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+mkdir delete
+cd delete
+
+jupyter nbconvert --to python --output-dir delete --execute ../src/chapters/chap01.ipynb
+jupyter nbconvert --to python --output-dir delete --execute ../src/chapters/chap02.ipynb
+jupyter nbconvert --to python --output-dir delete --execute ../src/chapters/chap03.ipynb
+jupyter nbconvert --to python --output-dir delete --execute ../src/chapters/chap04.ipynb
+jupyter nbconvert --to python --output-dir delete --execute ../src/chapters/chap05.ipynb
+jupyter nbconvert --to python --output-dir delete --execute ../src/chapters/chap06.ipynb
+jupyter nbconvert --to python --output-dir delete --execute ../src/chapters/chap07.ipynb
+jupyter nbconvert --to python --output-dir delete --execute ../src/chapters/chap08.ipynb
+jupyter nbconvert --to python --output-dir delete --execute ../src/chapters/chap09.ipynb
+jupyter nbconvert --to python --output-dir delete --execute ../src/chapters/chap10.ipynb
+jupyter nbconvert --to python --output-dir delete --execute ../src/chapters/chap11.ipynb
+jupyter nbconvert --to python --output-dir delete --execute ../src/chapters/chap12.ipynb
+jupyter nbconvert --to python --output-dir delete --execute ../src/chapters/chap13.ipynb
+jupyter nbconvert --to python --output-dir delete --execute ../src/chapters/chap14.ipynb
+jupyter nbconvert --to python --output-dir delete --execute ../src/chapters/chap15.ipynb
+jupyter nbconvert --to python --output-dir delete --execute ../src/chapters/chap16.ipynb
+jupyter nbconvert --to python --output-dir delete --execute ../src/chapters/chap17.ipynb
+jupyter nbconvert --to python --output-dir delete --execute ../src/chapters/chap18.ipynb
+jupyter nbconvert --to python --output-dir delete --execute ../src/chapters/chap19.ipynb
+jupyter nbconvert --to python --output-dir delete --execute ../src/chapters/chap20.ipynb
+jupyter nbconvert --to python --output-dir delete --execute ../src/chapters/chap21.ipynb
+jupyter nbconvert --to python --output-dir delete --execute ../src/chapters/chap22.ipynb
+jupyter nbconvert --to python --output-dir delete --execute ../src/chapters/chap23.ipynb
+
